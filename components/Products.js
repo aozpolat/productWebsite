229320087af9bfccc2afbc789627d27fd6d3466b
aozpolat <abdulkadirozpolatt@gmail.com>
2021-03-15 18:30:17 +0300

    addToCart button action

--- components/Products.js
+++ components/Products.js
@@ -1,96 +1,96 @@
 import Link from 'next/link'
+import { useList } from '../contexts/cartContext'
 
 export default function Products({products}) {
-return (
-    <div className="grid">
-        {products.map( (product, index) => (  
-                    <div className="card" key={index + 1}>
-                      <Link href={`/${index + 1}`}  > 
-                        <a> 
-                          <img src={product.imageUrl} alt=""></img>
-                          <h3>{product.name}    </h3>
-                          <h5>Click for more info</h5>
-                        </a>
-                      </Link>
-                      <div className="productContainer">
-                        <button onClick={() => addToCart({pid: product.pid, name: product.name, price: product.price})} className="addToCart">Add to Cart</button>
-                        <h3>${product.price}</h3>
-                      </div>
-                    </div>       
-                 
-        ))}
+    const {addToCart} = useList();
+    return (
+        <div className="grid">
+            {products.map( (product, index) => (  
+                        <div className="card" key={index + 1}>
+                        <Link href={`/${index + 1}`}  > 
+                            <a> 
+                            <img src={product.imageUrl} alt=""></img>
+                            <h3>{product.name}    </h3>
+                            <h5>Click for more info</h5>
+                            </a>
+                        </Link>
+                        <div className="productContainer">
+                            <button onClick={() => addToCart({pid: product.pid, name: product.name, price: product.price})} className="addToCart">Add to Cart</button>
+                            <h3>${product.price}</h3>
+                        </div>
+                        </div>       
+                    
+            ))}
 
 
-        <style jsx>{`
-            .grid {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-wrap: wrap;
-                max-width: 800px;
-                margin-top: 2rem;
-            }
+            <style jsx>{`
+                .grid {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    flex-wrap: wrap;
+                    max-width: 800px;
+                    margin-top: 2rem;
+                }
 
-            .card {
-                margin: 1rem;
-                flex-basis: 45%;
-                padding: 1.5rem;
-                color: inherit;
-                text-decoration: none;
-                border: 1px solid #eaeaea;
-                border-radius: 10px;
-                transition: color 0.15s ease, border-color 0.15s ease;
-              }
-    
-              .card:hover,
-              .card:focus,
-              .card:active {
-                color: #0070f3;
-                border-color: #0070f3;
-              }
-    
-              .card h3 {
-                margin: 0 0 1rem 0;
-                font-size: 1.5rem;
-              }
-    
-              .card p {
-                margin: 0;
-                font-size: 1.25rem;
-                line-height: 1.5;
-              }
-
-              a {
-                color: inherit;
-                text-decoration: none;
-              }
-
-              img {
-                width: 150px;
-                height: auto;
-                margin: auto;
-              }
-
-              .addToCart {
-                margin: 0.5rem 0;
-                padding: 0.4rem;
-                border-radius: 4px;
-                background: ivory;
-                cursor: pointer;
-              }
-    
-              .addToCart:hover {
-                background: #eeeecb
-              }
-    
-              .productContainer {
-                display: flex;
-                justify-content: space-between;
-              }
+                .card {
+                    margin: 1rem;
+                    flex-basis: 45%;
+                    padding: 1.5rem;
+                    color: inherit;
+                    text-decoration: none;
+                    border: 1px solid #eaeaea;
+                    border-radius: 10px;
+                    transition: color 0.15s ease, border-color 0.15s ease;
+                }
         
-        `}</style>
-    </div>
-)
+                .card:hover,
+                .card:focus,
+                .card:active {
+                    color: #0070f3;
+                    border-color: #0070f3;
+                }
+        
+                .card h3 {
+                    margin: 0 0 1rem 0;
+                    font-size: 1.5rem;
+                }
+        
+                .card p {
+                    margin: 0;
+                    font-size: 1.25rem;
+                    line-height: 1.5;
+                }
 
+                a {
+                    color: inherit;
+                    text-decoration: none;
+                }
 
+                img {
+                    width: 150px;
+                    height: auto;
+                    margin: auto;
+                }
+
+                .addToCart {
+                    margin: 0.5rem 0;
+                    padding: 0.4rem;
+                    border-radius: 4px;
+                    background: ivory;
+                    cursor: pointer;
+                }
+        
+                .addToCart:hover {
+                    background: #eeeecb
+                }
+        
+                .productContainer {
+                    display: flex;
+                    justify-content: space-between;
+                }
+            
+            `}</style>
+        </div>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -17,70 +17,68 @@ export async function getStaticProps() {
 }
 
 export default function Home({products}) {
-  const [currentProducts, setCurrentProducts] = useState([]);
-  const [currentPage, setCurrenPage] = useState(1);
-  const [productsPerPage, setProductsPerPage] = useState(10);
+    const [currentProducts, setCurrentProducts] = useState([]);
+    const [currentPage, setCurrenPage] = useState(1);
+    const [productsPerPage, setProductsPerPage] = useState(10);
 
-  const {cart, addToCart} = useList();
+    const {cart} = useList();
 
-
-
-  return (
-    <Layout home cart={cart}>
-      <div className="container">
-        <Head>       
-          <title>Products</title>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"/>     
-        </Head>
-        <main>
-            <h1 className="title">
-              Welcome 
-            </h1>  
-            <Products products= {products}/>
+    return (
+        <Layout home cart={cart}>
+            <div className="container">
+            <Head>       
+                <title>Products</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"/>     
+            </Head>
+            <main>
+                <h1 className="title">
+                    Welcome 
+                </h1>  
+                <Products products= {products}/>
+                
+            </main>
             
-        </main>
-        
 
-        <style jsx>{`
-          .container {
-            min-height: 100vh;
-            padding: 0 0.5rem;
-            display: flex;       
-            justify-content: center;   
-          }
+            <style jsx>{`
+                .container {
+                min-height: 100vh;
+                padding: 0 0.5rem;
+                display: flex;       
+                justify-content: center;   
+                }
 
-          main {
-            padding: 1rem;
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            
-          }
+                main {
+                padding: 1rem;
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                
+                }
 
-          .title {
-            margin: 0;
-            line-height: 1.15;
-            font-size: 4rem;
-          }
+                .title {
+                margin: 0;
+                line-height: 1.15;
+                font-size: 4rem;
+                }
 
-          .title {
-            text-align: center;
-          }
+                .title {
+                text-align: center;
+                }
 
-          @media (max-width: 600px) {
-            .grid {
-              width: 100%;
-              flex-direction: column;
-            }
-          }
+                @media (max-width: 600px) {
+                .grid {
+                    width: 100%;
+                    flex-direction: column;
+                }
+                }
 
-        `}</style>
-      </div>
-    </Layout>
-  )
+            `}</style>
+            </div>
+        </Layout>
+    )
 }
 
