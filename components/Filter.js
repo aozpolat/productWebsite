d6a195a0d63d58d29a4b2551241e2ac721417d38
aozpolat <abdulkadirozpolatt@gmail.com>
2021-03-19 17:55:13 +0300

    added first steps of router formatting for filter system

--- components/Filter.js
+++ components/Filter.js
@@ -1,132 +1,161 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { useRouter } from "next/router";
+export default function Filter({ filter, removeCategoryFilter }) {
+  const router = useRouter();
+  return (
+    <div className="filter">
+      <div className="headline">
+        <h3>Categories</h3>
+      </div>
 
-export default function Filter() {
-    return (
-        <div className="filter">
-            <div className="headline">
-                <h3>Categories</h3>
-            </div>
-            
-            <div className="categories">
-                <div className="option">
-                    <input type='checkbox' ></input>
-                    <label> Shoes</label><br></br>
-                </div>
-                <div className="option">
-                    <input type='checkbox' ></input>
-                    <label> Dresses</label><br></br>
-                </div>
-                <div className="option">
-                    <input type='checkbox' ></input>
-                    <label> Trousers</label><br></br>
-                </div>
-                <div className="option">
-                    <input type='checkbox' ></input>
-                    <label> Jackets</label><br></br>
-                </div>
-                <div className="option">
-                    <input type='checkbox' ></input>
-                    <label> Shirts</label><br></br>
-                </div>        
-            </div>
-            <div className="headline">
-                <h3>Price</h3>
-            </div>
-            <div className="price">
-                <div className="priceBlock">
-                    <div className="priceSpan">
-                        <input type="number" placeholder="Min"></input>                  
-                    </div>
-                    <div className="range"> - </div>
-                    <div className="priceSpan">
-                        <input type="number" placeholder="Max"></input>
-                    </div>
-                </div>
-                <button><FontAwesomeIcon icon={faArrowRight} size="lg"/></button>      
-            </div>
-
-            <style jsx>{`
-                .filter {
-                    max-height: 23rem;
-                    width: 12rem;
-                    margin: 7rem 0 0 4rem;
-                    border: 1px solid #e5e5e5;
-                    border-radius: 5px;
-                }
-
-                .headline {
-                    
-                    border-bottom: 1px solid;
-                }
-
-                .headline h3 {
-                    margin: 0.7em;
-                }
-
-                .categories {
-                    display:flex;
-                    flex-direction: column;
-                    
-                }
-
-                .option {
-                    display:flex;
-                    align-items: center;
-                    margin: 0.3rem 1rem;
-                }
-
-                .option input {
-                    margin-right: 0.5rem;
-                    height: 20px;
-                    width: 20px;
-                    cursor: pointer;
-                }
-
-                .price {
-                    display:flex;
-                }
-
-                .priceBlock {
-                    display:flex;
-                    margin-left: 0.4rem;
-                }
-                .priceSpan, .range {    
-                    margin: 1rem 0.25rem;
-                    
-                }
-
-                .priceSpan input {
-                    margin-left: 0.2rem;
-                    width: 3rem;
-                    padding: 6px 8px;
-                    border-radius: 4px;
-                    border: 2px solid #bfbfbf;
-                }
-
-                /* Chrome, Safari, Edge, Opera */
-                input::-webkit-outer-spin-button,
-                input::-webkit-inner-spin-button {
-                -webkit-appearance: none;
-                margin: 0;
-                }
-
-                /* Firefox */
-                input[type=number] {
-                -moz-appearance: textfield;
-                }
-
-                .price button {
-                    margin: 1rem 0.7rem;
-                    background: #e9e0bf;
-                }
-
-                .price button:hover {
-                    background: #e9d486;
-                    cursor: pointer;
-                }
-
-            `}</style>
+      <div className="categories">
+        <div className="option">
+          <input
+            type="checkbox"
+            onChange={() => {
+              if (
+                router.query.category &&
+                router.query.category.includes("shoes")
+              )
+                removeCategoryFilter("shoes");
+              else filter("shoes");
+            }}
+            checked={
+              router.query.category
+                ? router.query.category.includes("shoes")
+                : false
+            }
+          ></input>
+          <label> Shoes</label>
+          <br></br>
         </div>
-    )
+        <div className="option">
+          <input
+            type="checkbox"
+            onChange={() => filter("dresses")}
+            checked={
+              router.query.category
+                ? router.query.category.includes("dresses")
+                : false
+            }
+          ></input>
+          <label> Dresses</label>
+          <br></br>
+        </div>
+        <div className="option">
+          <input type="checkbox"></input>
+          <label> Trousers</label>
+          <br></br>
+        </div>
+        <div className="option">
+          <input type="checkbox"></input>
+          <label> Jackets</label>
+          <br></br>
+        </div>
+        <div className="option">
+          <input type="checkbox"></input>
+          <label> Shirts</label>
+          <br></br>
+        </div>
+      </div>
+      <div className="headline">
+        <h3>Price</h3>
+      </div>
+      <div className="price">
+        <div className="priceBlock">
+          <div className="priceSpan">
+            <input type="number" placeholder="Min"></input>
+          </div>
+          <div className="range"> - </div>
+          <div className="priceSpan">
+            <input type="number" placeholder="Max"></input>
+          </div>
+        </div>
+        <button>
+          <FontAwesomeIcon icon={faArrowRight} size="lg" />
+        </button>
+      </div>
+
+      <style jsx>{`
+        .filter {
+          max-height: 23rem;
+          width: 12rem;
+          margin: 7rem 0 0 4rem;
+          border: 1px solid #e5e5e5;
+          border-radius: 5px;
+        }
+
+        .headline {
+          border-bottom: 1px solid;
+        }
+
+        .headline h3 {
+          margin: 0.7em;
+        }
+
+        .categories {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .option {
+          display: flex;
+          align-items: center;
+          margin: 0.3rem 1rem;
+        }
+
+        .option input {
+          margin-right: 0.5rem;
+          height: 20px;
+          width: 20px;
+          cursor: pointer;
+        }
+
+        .price {
+          display: flex;
+        }
+
+        .priceBlock {
+          display: flex;
+          margin-left: 0.4rem;
+        }
+        .priceSpan,
+        .range {
+          margin: 1rem 0.25rem;
+        }
+
+        .priceSpan input {
+          margin-left: 0.2rem;
+          width: 3rem;
+          padding: 6px 8px;
+          border-radius: 4px;
+          border: 2px solid #bfbfbf;
+        }
+
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+          -webkit-appearance: none;
+          margin: 0;
+        }
+
+        /* Firefox */
+        input[type="number"] {
+          -moz-appearance: textfield;
+        }
+
+        .price button {
+          margin: 1rem 0.7rem;
+          background: #e9e0bf;
+        }
+
+        .price button:hover {
+          background: #e9d486;
+          cursor: pointer;
+        }
+      `}</style>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,116 @@
-import Head from 'next/head'
-import {getProductsForPage} from '../formElements/api'
-import Layout from '../components/layout'
-import Products from '../components/Products'
-import Pagination from '../components/Pagination'
-import { useRouter } from 'next/router'
-import Filter from '../components/Filter'
+import Head from "next/head";
+import { getProductsForPage } from "../formElements/api";
+import Layout from "../components/layout";
+import Products from "../components/Products";
+import Pagination from "../components/Pagination";
+import { useRouter } from "next/router";
+import Filter from "../components/Filter";
 
-export async function getServerSideProps({ query: { page = 1, category} }) {
-    const products = await getProductsForPage(page);
-    return {
-        props: {
-            currentPage:page,
-            products,
-        }
-    }
+export async function getServerSideProps({
+  query: { page = 1, category = "all" },
+}) {
+  const products = await getProductsForPage(page);
+  console.log(category);
+  return {
+    props: {
+      currentPage: page,
+      products,
+    },
+  };
 }
 
-export default function Home({products, currentPage}) {
-    const router = useRouter()
-    const pageNumber = Math.ceil(products.length / products.productsPerPage);
+export default function Home({ products, currentPage }) {
+  const router = useRouter();
+  const pageNumber = Math.ceil(products.length / products.productsPerPage);
 
-    const paginate = (number) => {
-        router.push(`/?page=${number}`);
+  const paginate = (number) => {
+    router.push(`/?page=${number}`);
+  };
+  const removeCategoryFilter = (category) => {
+    if (!Array.isArray(router.query.category))
+      router.push({
+        pathname: "/",
+        query: { category: [] },
+      });
+    else {
+      const newArr = router.query.category.filter((item) => item !== category);
+      router.push({
+        pathname: "/",
+        query: { category: newArr },
+      });
     }
-    return (
-        <Layout home >
-            <div className="container">
-                <Head>       
-                    <title>Products</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"/>     
-                </Head>
+  };
+  const filter = (category) => {
+    const currentPath = router.asPath;
+    if (router.query.category)
+      router.push(`${currentPath}&category=${category}`);
+    else {
+      router.push({
+        pathname: "/",
+        query: { ...router.query, category },
+      });
+    }
+  };
+  return (
+    <Layout home>
+      <div className="container">
+        <Head>
+          <title>Products</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
 
-                <Filter />
-                <main>
-                    <h1 className="title">
-                        Welcome 
-                    </h1>  
-                    <Products products = {products.currentProducts}/>
-                    <Pagination  pageNumber = {pageNumber} paginate = {paginate} currentPage = {currentPage} />               
-                </main>
-                
+        <Filter filter={filter} removeCategoryFilter={removeCategoryFilter} />
+        <main>
+          <h1 className="title">Welcome</h1>
+          <Products products={products.currentProducts} />
+          <Pagination
+            pageNumber={pageNumber}
+            paginate={paginate}
+            currentPage={currentPage}
+          />
+        </main>
 
-                <style jsx>{`
-                    .container {
-                    min-height: 100vh;
-                    padding: 0 0.5rem;
-                    display: flex;       
-                    justify-content: center;
-                    }
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            justify-content: center;
+          }
 
-                    main {
-                    padding: 1rem;
-                    margin-right: 5rem;
-                    flex: 1;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                    
-                    }
+          main {
+            padding: 1rem;
+            margin-right: 5rem;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
 
-                    .title {
-                    margin: 0;
-                    line-height: 1.15;
-                    font-size: 4rem;
-                    }
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
 
-                    .title {
-                    text-align: center;
-                    }
+          .title {
+            text-align: center;
+          }
 
-                    @media (max-width: 600px) {
-                    .grid {
-                        width: 100%;
-                        flex-direction: column;
-                    }
-                    }
-
-                    
-                `}</style>
-            </div>
-        </Layout>
-    )
+          @media (max-width: 600px) {
+            .grid {
+              width: 100%;
+              flex-direction: column;
+            }
+          }
+        `}</style>
+      </div>
+    </Layout>
+  );
 }
-
